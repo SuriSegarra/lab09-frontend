0de15ea437a210c5d82567c6679f6bb2bd6ff7f8
surelis segarra <sdarianne34@gmail.com>
2020-02-21 10:57:01 -0800

    getting api data in list component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import {
   Link,
   BrowserRouter as Router,
 } from 'react-router-dom';
+import List from './components/List.js'
+
+
+
 export default class App extends Component {
   render() {
     return (
       
         <Router>
-          <Link to='/'>HomePage </Link>
+          {/* <Link to='/'>HomePage </Link> */}
           <Link to='/list'>Weed</Link>
           <Switch>
             {/* this is your home route */}
 
-            <Route exact path='/list' compoment={List}/>
+            <Route exact path='/list' component={List}/>
             {/* home esta abajo de list porque va solo leer / solo y no la L desoues de el / de list */}
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/detail:_strain' compoment={Detail}/>
+            {/* <Route exact path='/' component={Home}/> */}
+            {/* <Route exact path='/detail:_strain' compoment={Detail}/> */}
           </Switch>
         </Router>
       
--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 import request from 'superagent';
 import { Link } from 'react-router-dom';
-import WeedItem from './WeedItem.js';
+// import WeedItem from './WeedItem.js';
+
+
+const getWeedList = () => request.get(`http://weed-fullstack-lab.herokuapp.com/api/weed`);
+
 export default class List extends Component {
+
+    state = {
+        weed: [],
+    }
+
+    async componentDidMount() {
+        const data = await getWeedList()
+        console.log(data.body);
+        this.setState({
+            weed: data.body
+        })
+        
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
-        )
+           <div>hello</div>
+        );
     }
 }
