ff11b2fa0c0b9ae7203268184eb47d44628ad855
surelis segarra <sdarianne34@gmail.com>
2020-02-21 10:41:44 -0800

    fixing repo problem

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {
+  Route, 
+  Switch,
+  Link,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+export default class App extends Component {
+  render() {
+    return (
+      
+        <Router>
+          <Link to='/'>HomePage </Link>
+          <Link to='/list'>Weed</Link>
+          <Switch>
+            {/* this is your home route */}
+
+            <Route exact path='/list' compoment={List}/>
+            {/* home esta abajo de list porque va solo leer / solo y no la L desoues de el / de list */}
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/detail:_strain' compoment={Detail}/>
+          </Switch>
+        </Router>
+      
+    )
+  }
+}
+
+
+
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import request from 'superagent';
+import { Link } from 'react-router-dom';
+import WeedItem from './WeedItem.js';
+export default class List extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
