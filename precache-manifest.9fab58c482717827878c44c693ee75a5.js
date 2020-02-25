6885aa19edd016bcdffee02a9b5f2f39ae42260f
surelis segarra <sdarianne34@gmail.com>
2020-02-25 10:45:29 -0800

    Updates

---
+++ precache-manifest.9fab58c482717827878c44c693ee75a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc483adf1a33b68725c23ba90eb64977",
+    "url": "/lab09-frontend/index.html"
+  },
+  {
+    "revision": "f0aae47a232c6e993855",
+    "url": "/lab09-frontend/static/css/main.2156e697.chunk.css"
+  },
+  {
+    "revision": "0d144608353bc537d213",
+    "url": "/lab09-frontend/static/js/2.57f3a995.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/lab09-frontend/static/js/2.57f3a995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0aae47a232c6e993855",
+    "url": "/lab09-frontend/static/js/main.13bdec10.chunk.js"
+  },
+  {
+    "revision": "bee37344ac846495596e",
+    "url": "/lab09-frontend/static/js/runtime-main.84996341.js"
+  },
+  {
+    "revision": "2b5d84cd1f4d9b00716771fd75401195",
+    "url": "/lab09-frontend/static/media/logo1.2b5d84cd.png"
+  }
+]);
